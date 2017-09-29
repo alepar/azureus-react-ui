8c8e5e41a221ba655ffd07569d925850d9e41438
Alexey Parfenov <parfenov@amazon.com>
2017-09-29 13:33:34 -0700

    4.2 dummy user list table

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
 import React from "react";
-import "../stylesheets/main.scss";
 
-// app component
 export default class App extends React.Component {
-  // render
-  render() {
-    return (
-      <div className="container">
-        {this.props.children}
-      </div>
-    );
-  }
+
+    render() {
+        const users = [];
+        for (let i=1; i<10; i++) {
+            users.push({
+                id: i,
+                username: "John " + i,
+                job: "Some job " + i,
+            });
+        }
+
+        return (
+            <table>
+                <thead><tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Job</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr></thead>
+                <tbody>
+                    {users.map((user, idx) => {
+                        return (
+                            <tr key={user.id}>
+                                <td>#{user.id}</td>
+                                <td>{user.username}</td>
+                                <td>{user.job}</td>
+                                <td><a href={'/user-edit/' + user.id}>Edit</a></td>
+                                <td><button data-id={user.id}>Delete</button></td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        );
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./store.js";
-import { router } from "./router.js";
+import App from "./components/App";
 
 // render the main component
 ReactDOM.render(
-  <Provider store={store}>
-    {router}
-  </Provider>,
-  document.getElementById('app')
+    <App/>,
+    document.getElementById('app')
 );
