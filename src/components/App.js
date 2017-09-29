36d40cd6a5edff3bf40b7f9abd019ecc70162307
Alexey Parfenov <parfenov@amazon.com>
2017-09-29 13:58:36 -0700

    4.3 breaking table into UserList/UserListElement components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import UserList from "./UserList";
 
 export default class App extends React.Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+
         const users = [];
         for (let i=1; i<10; i++) {
             users.push({
@@ -12,29 +15,14 @@ export default class App extends React.Component {
             });
         }
 
+        this.state = {
+            users: users,
+        };
+    }
+
+    render() {
         return (
-            <table>
-                <thead><tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Job</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr></thead>
-                <tbody>
-                    {users.map((user, idx) => {
-                        return (
-                            <tr key={user.id}>
-                                <td>#{user.id}</td>
-                                <td>{user.username}</td>
-                                <td>{user.job}</td>
-                                <td><a href={'/user-edit/' + user.id}>Edit</a></td>
-                                <td><button data-id={user.id}>Delete</button></td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <UserList users={this.state.users}/>
         );
     }
 
