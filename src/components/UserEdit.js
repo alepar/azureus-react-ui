2e29429f395f58902a125109b4d23782f05ed1d0
Alexey Parfenov <parfenov@amazon.com>
2017-10-02 13:22:31 -0700

    7.1 add/edit form layout

--- src/components/UserEdit.js
+++ src/components/UserEdit.js
@@ -1,12 +1,52 @@
 import React from "react";
-import { PageHeader } from "react-bootstrap";
+import { PageHeader, Form, FormGroup, Col, Button, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
+import { Field, reduxForm } from "redux-form";
 
-export default class UserEdit extends React.Component {
+class UserEdit extends React.Component {
 
     render() {
         return (<div>
             <PageHeader>User add/edit</PageHeader>
-            Some form here
+            <Form horizontal>
+                <Field name={"username"} component={UserEdit.renderUsername}/>
+                <Field name={"job"} component={UserEdit.renderJob}/>
+                <FormGroup>
+                    <Col smOffset={2} sm={8}>
+                        <Button type={"submit"} >Save User</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
         </div>);
     }
+
+    static renderUsername(props) {
+        return (
+            <FormGroup>
+                <Col sm={2}>Username</Col>
+                <Col sm={8}>
+                    <FormControl {...props.input} id={"username"} type={"text"} placeholder={"Username"}/>
+                </Col>
+            </FormGroup>
+        );
+    }
+
+    static renderJob(props) {
+        return (
+            <FormGroup>
+                <Col sm={2}>Job</Col>
+                <Col sm={8}>
+                    <InputGroup>
+                        <FormControl {...props.input} id={"job"} type={"text"} placeholder={"Job"}/>
+                        <InputGroup.Addon>
+                            <Glyphicon glyph={"briefcase"}/>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </Col>
+            </FormGroup>
+        );
+    }
 }
+
+export default UserEdit = reduxForm({
+    form: "user_edit",
+})(UserEdit);
